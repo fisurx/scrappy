b64bca6a7f6c31704540a032b76082d9a45f5e31
fisurx <davinchocastro@gmail.com>
2020-01-22 00:24:35 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- coding: 850 -*-
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+
+#route
+url = "http://www.bolsamadrid.es/esp/aspx/Indices/Resumen.aspx"
+
+page = requests.get(url).text
+soup = BeautifulSoup(page, "lxml")
+
+#get
+tabla = soup.find('table', attrs={'id': 'ctl00_Contenido_tblÍndices'})
+
+name=""
+price=""
+nroFila=0
+for fila in tabla.find_all("tr"):
+    if nroFila==1:
+        nroCelda=0
+        for celda in fila.find_all('td'):
+            if nroCelda==0:
+                name=celda.text
+                print("Indice:", name)
+            if nroCelda==2:
+                price=celda.text
+                print("Valor:", price)
+            nroCelda=nroCelda+1
+    nroFila=nroFila+1
